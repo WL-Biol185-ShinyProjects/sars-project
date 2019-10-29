cfb72f22173dfaf19f6fadbcd6c963b967530912
kimj20 <kimj20@mail.wlu.edu>
2019-10-29 17:37:07 -0400

    fix version update 10/29

--- ui.r
+++ ui.r
@@ -1,27 +1,49 @@
 #Load all libraries here
 library(shiny)
 library(shinydashboard)
+library(leaflet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Page Aesthetics
-dashboardPage(
-  dashboardHeader(title = "The 2003 SARs Epidemic"),
+dashboardPage(skin = "red",
+  dashboardHeader(title = "The 2003 SARs Epidemic",titleWidth = 300),
   #content to put in our sidebar
-  dashboardSidebar(
+  dashboardSidebar(width = 300,
       sidebarMenu(
         menuItem("Introduction", tabName = "introduction", icon = icon("info-circle", lib = "font-awesome")),
-        menuItem("TotalCases", tabName = "totalcases", icon = icon("dashboard")),
-        menuItem("Recoveries", tabName = "recoveries", icon = icon("th"))),
+        menuItem("Place", tabName = "place", icon = icon ("map-marked-alt", lib = "font-awesome")),
+        menuItem("Time", tabName = "time", icon = icon ("clock", lib = "font-awesome")),
+        menuItem("TotalCases", tabName = "totalcases", icon = icon("list-ol", lib = "font-awesome")),
+        menuItem("Recoveries", tabName = "recoveries", icon = icon("clinic-medical", lib = "font-awesome")))
       ),
   
   dashboardBody(
     tabItems(
         tabItem(tabName = "introduction",
                 h2("intro tab content")),
+        tabItem(tabName = "place",
+                h2("place map content")),
+        tabItem(tabName = "introduction",
+                h2("time map content")),
         tabItem(tabName = "totalcases",
                 h2("total cases content")),
         tabItem(tabName = "recoveries",
                 h2("recoveries content")))
   )
-
+)
 
 
