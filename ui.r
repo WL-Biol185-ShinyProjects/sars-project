cc0f702726f4a17ae6b91ad5be0384ee531d60d3
kimj20 <kimj20@mail.wlu.edu>
2019-10-29 12:53:07 -0400

    will figure out later. 10/29

--- ui.r
+++ ui.r
@@ -10,29 +10,29 @@ dashboardPage(
   #content to put in our sidebar
   dashboardSidebar(
       sidebarMenu(
+        menuItem("Introduction", tabName = "introduction", icon = icon("info-circle", lib = "font-awesome")),
         menuItem("TotalCases", tabName = "totalcases", icon = icon("dashboard")),
         menuItem("Recoveries", tabName = "recoveries", icon = icon("th"))
       )
     ),
   #content to put in the body
-  dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "totalcases",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
+    # Second tab content
+  tabItem(tabName = "introduction",
+          h2("intro tab content")
+  ),
+          
+  tabItem(tabName = "totalcases",
+            fluidRow(
+               box(plotOutput("plot1", height = 250)),
                 
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+              box(
+                 title = "Controls",
+                 sliderInput("slider", "Number of observations:", 1, 100, 50)
           )
         )
       ),
-      
-      # Second tab content
+    
       tabItem(tabName = "recoveries",
               h2("Recoveries tab content")
      )
     )
-  )
-)
