66e1f780d91c9bb68dca580371820e4e26acad4e
kimj20 <kimj20@mail.wlu.edu>
2019-10-29 18:13:24 -0400

    Update Final 10/29

--- server.r
+++ server.r
@@ -1,6 +1,17 @@
 #this is where our server code goes
 library(shiny)
+library(leaflet)
 
-
-function(input, output) {
-} 
+function(input, output, session) {
+    
+    lats <- -90:90
+    lons <- -180:180
+    
+    output$worldMap <- renderLeaflet({
+      
+      leaflet() %>% 
+        setView(lng = 0, lat = 10, zoom = 2) %>% 
+        addTiles()
+    })
+    
+  }
--- ui.r
+++ ui.r
@@ -20,13 +20,12 @@ library(leaflet)
 
 #Page Aesthetics
 dashboardPage(skin = "red",
-  dashboardHeader(title = "The 2003 SARs Epidemic",titleWidth = 300),
+  dashboardHeader(title = "The 2003 SARs Epidemic: Jenna Kim and Tiffany Ko",titleWidth = 500),
   #content to put in our sidebar
-  dashboardSidebar(width = 300,
+  dashboardSidebar(width = 200,
       sidebarMenu(
         menuItem("Introduction", tabName = "introduction", icon = icon("info-circle", lib = "font-awesome")),
-        menuItem("Place", tabName = "place", icon = icon ("map-marked-alt", lib = "font-awesome")),
-        menuItem("Time", tabName = "time", icon = icon ("clock", lib = "font-awesome")),
+        menuItem("Time and Place", tabName = "timeandplace", icon = icon ("map-marked-alt", lib = "font-awesome")),
         menuItem("TotalCases", tabName = "totalcases", icon = icon("list-ol", lib = "font-awesome")),
         menuItem("Recoveries", tabName = "recoveries", icon = icon("clinic-medical", lib = "font-awesome")))
       ),
@@ -34,16 +33,19 @@ dashboardPage(skin = "red",
   dashboardBody(
     tabItems(
         tabItem(tabName = "introduction",
-                h2("intro tab content")),
-        tabItem(tabName = "place",
-                h2("place map content")),
-        tabItem(tabName = "introduction",
-                h2("time map content")),
+                h2("place introductory elements here")),
+        
+        tabItem(tabName = "timeandplace",
+                navbarPage("Time and Place of the Epidemic",
+                           tabPanel("Time",
+                                    fluidRow(
+                                       (leafletOutput("worldMap", height = 400, width = 700)))),
+                           tabPanel("Place"))),
+        
         tabItem(tabName = "totalcases",
                 h2("total cases content")),
         tabItem(tabName = "recoveries",
                 h2("recoveries content")))
   )
 )
-
 
