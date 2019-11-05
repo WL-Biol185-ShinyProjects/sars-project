9dc4341a1095419fa467d926c07e2d75b12c5ea2
kot20 <kot20@mail.wlu.edu>
2019-11-05 13:11:04 -0500

    record of leaflet idea for timeMap

--- Visualization ideas.R
+++ Visualization ideas.R
@@ -1,3 +1,7 @@
 #Script file to test visualization ideas (leaflet, ggplot, etc)
 library(ggplot2)
 library(leaflet)
+leaflet(data = tidySARSdata) %>% 
+  setView(lng = 0, lat = 10, zoom = 2) %>% 
+  addTiles() %>% 
+  addMarkers(~longitude, ~latitude)
