4bf81fd6cd7e7abb3637bd2ec3fbe94e8c333838
kot20 <kot20@mail.wlu.edu>
2019-11-07 13:19:06 -0500

    Dynamic slider that will filter based on first onset dates for Time of Epidemic map
    Added creation of tidySARSdata to UI script. May want to create a global.r file for it next time

--- server.r
+++ server.r
@@ -4,11 +4,13 @@ function(input, output, session) {
   lons <- -180:180
     
   output$timeMap <- renderLeaflet({
-    leaflet(data = tidySARSdata) %>% 
+    tidySARSdata %>% 
+      filter(firstOnset <= input$dateSlider) %>% 
+      leaflet() %>% 
       setView(lng = 0, lat = 10, zoom = 2) %>% 
       addTiles() %>% 
       addMarkers(~longitude, ~latitude)
-    })
-    
+  })
 }
+
 
