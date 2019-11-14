650039c622f92c64fafc37e4eff200ea5cc2c458
kimj20 <kimj20@mail.wlu.edu>
2019-11-14 12:00:56 -0500

    11/14.1

--- ui.r
+++ ui.r
@@ -46,15 +46,6 @@ names(tidySARSdata) <- c("latitude", "longitude", "areas", "female", "male", "to
                          "medianAge", "youngestCase", "oldestCase", "currentlyHospitalized", "
                          casesRecovered", "deaths", "caseFatalityRate", "importedCases", 
                          "percentImportedcases", "affectedHCW", "percentHCW", "firstOnset", "lastOnset")
-
-#HDI2003 <- read_csv("HDI2003.csv", col_names = TRUE,
-                    add_case(HDI2003, Country = c("Macao, China (SAR)", "Taiwan, China (SAR)"), HDI = c("N/A", "N/A"),
-                             filter(HDI2003, Country == c("Australia", "Brazil", "Canada", "China", "Hong Kong, China (SAR)",
-                                                          "Macao, China (SAR)", "Taiwan, China (SAR)", "Colombia", "Finland", 
-                                                          "France", "Germany", "India", "Indonesia", "Italy", "Kuwait", 
-                                                          "Malaysia", "Mongolia", "New Zealand", "Philippines", "Ireland", 
-                                                          "Korea (Republic of)", "Romania", "Russian Federation", "Singapore", "South Africa",
-                                                          "Spain", "Sweden", "Switzerland", "Thailand", "United Kingdom", "United States", "Viet Nam"))
                             
 
 #Page Aesthetics
